f0e8d88624757300f30e4bb0693401d80073fd6b
Marcos Bernal <marcos.bernal@casumo.com>
2022-12-01 18:23:19 +0100

    Day 1. Iterative solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
 fun main() {
+    fun asListOfListOfInts(input: List<String>): List<List<Int>> {
+        val accumlatorList = mutableListOf<MutableList<String>>()
+        accumlatorList.add(mutableListOf())
+        var currentIndex = 0
+        input.forEach {
+            if (it == "") {
+                currentIndex++
+    //                println("currentIndex = ${currentIndex}, it = ${it}, templist = $templist")
+                accumlatorList.add(mutableListOf())
+            } else {
+    //                println("currentIndex = ${currentIndex}, it = ${it}, templist = $templist")
+                accumlatorList[currentIndex].add(it)
+            }
+        }
+        return accumlatorList.map { it.map { it.toInt() }.toList() }.toList()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return asListOfListOfInts(input).map { it.sum() }.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val summedList = asListOfListOfInts(input).map { it.sum() }
+        val sortedList = summedList.sortedDescending()
+        println("sortedList = ${sortedList}")
+
+        val trimmedList = sortedList.take(3)
+        println("trimmedList = ${trimmedList}")
+        return trimmedList.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("solution for your input, part 1 = ${part1(input)}")
+    println("solution for your input, part 2 = ${part2(input)}")
 }
