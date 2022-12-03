562ddd1ca6e479a328dd1ce66f887cc4393d788a
Marcos Bernal <marcos.bernal@casumo.com>
2022-12-03 14:28:19 +0100

    Day 2 - Solution to part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,16 +1,47 @@
+enum class OpponentChoice(val code: String) {
+    Rock("A"),
+    Paper("B"),
+    Scissors("C");
+
+    companion object {
+        infix fun from(value: String): OpponentChoice = OpponentChoice.values().firstOrNull { it.code == value }!!
+    }
+}
+
+enum class ResponseChoice(val code: String) {
+    Rock("X"),
+    Paper("Y"),
+    Scissors("Z");
+
+    companion object {
+        infix fun from(value: String): ResponseChoice = ResponseChoice.values().firstOrNull { it.code == value }!!
+    }
+}
+
+enum class ExpectedAction(val code: String) {
+    Lose("X"),
+    Draw("Y"),
+    Win("Z");
+
+    companion object {
+        infix fun from(value: String): ExpectedAction = ExpectedAction.values().firstOrNull { it.code == value }!!
+    }
+}
+
 typealias Round = Pair<OpponentChoice, ResponseChoice>
+typealias Strategy = Pair<OpponentChoice, ExpectedAction>
 
 fun main() {
 
 
-    fun transformInput(input: List<String>): List<Round> {
-//        println(input)
-//        return listOf(
-//            Pair(OpponentChoice.from("A"), ResponseChoice.from("Y")),
-//            Pair(OpponentChoice.from("B"), ResponseChoice.from("X")),
-//            Pair(OpponentChoice.from("C"), ResponseChoice.from("Z")),
-//        )
-        return input.map { it.split(" ") }.map { Pair(OpponentChoice.from(it[0]), ResponseChoice.from(it[1]))}
+    fun inputSplitted(input: List<String>) = input.map { it.split(" ") }
+
+    fun transformInputPart1(input: List<String>): List<Round> {
+        return inputSplitted(input).map { Pair(OpponentChoice.from(it[0]), ResponseChoice.from(it[1])) }
+    }
+
+    fun transformInputPart2(input: List<String>): List<Strategy> {
+        return inputSplitted(input).map { Pair(OpponentChoice.from(it[0]), ExpectedAction.from(it[1])) }
     }
 
     fun shapeScore(choice: ResponseChoice) = when (choice) {
@@ -45,23 +76,42 @@ fun main() {
         }
     }
 
-    fun part2PredictingScore(round: Round): Int {
-        return outcomeScore(round)
-    }
-
     fun singleRoundScorePart1(round: Round): Int = outcomeScore(round) + shapeScore(round.second)
-    fun singleRoundScorePart2(round: Round): Int = part2PredictingScore(round) + shapeScore(round.second) // not really, shapescore is wrong
 
     fun part1(input: List<String>): Int {
-        val transformed = transformInput(input)
+        val transformed = transformInputPart1(input)
         val allScores = transformed.map { singleRoundScorePart1(it) }
         return allScores.sum()
     }
 
+    fun playRound(strategy: Pair<OpponentChoice, ExpectedAction>): Pair<OpponentChoice, ResponseChoice> {
+        val choice = when (strategy.second) {
+            ExpectedAction.Lose -> when (strategy.first) {
+                OpponentChoice.Rock -> ResponseChoice.Scissors
+                OpponentChoice.Paper -> ResponseChoice.Rock
+                OpponentChoice.Scissors -> ResponseChoice.Paper
+            }
+
+            ExpectedAction.Draw -> when (strategy.first) {
+                OpponentChoice.Rock -> ResponseChoice.Rock
+                OpponentChoice.Paper -> ResponseChoice.Paper
+                OpponentChoice.Scissors -> ResponseChoice.Scissors
+            }
+
+            ExpectedAction.Win -> when (strategy.first) {
+                OpponentChoice.Rock -> ResponseChoice.Paper
+                OpponentChoice.Paper -> ResponseChoice.Scissors
+                OpponentChoice.Scissors -> ResponseChoice.Rock
+            }
+        }
+        return Pair(strategy.first, choice)
+    }
+
     fun part2(input: List<String>): Int {
-        val transformed = transformInput(input)
-        val allScores = transformed.map { singleRoundScorePart2(it) }
-        return input.size
+        val strategies = transformInputPart2(input)
+        val rounds = strategies.map { playRound(it) }
+        val allScores = rounds.map { singleRoundScorePart1(it) }
+        return allScores.sum()
     }
 
     // test if implementation meets criteria from the description, like:
@@ -69,7 +119,7 @@ fun main() {
     val testResult = part1(testInput)
     val testResult2 = part2(testInput)
     check(testResult == 15) { "wrong input loco $testResult is not 15" }
-    check(testResult2 == 12)
+    check(testResult2 == 12) { "wrong input loco $testResult is not 12" }
 
     val input = readInput("Day02")
     val result1 = part1(input)
@@ -78,22 +128,3 @@ fun main() {
     println("solution for your input, part 2 = $result2")
 }
 
-enum class OpponentChoice(val code: String) {
-    Rock("A"),
-    Paper("B"),
-    Scissors("C");
-
-    companion object {
-        infix fun from(value: String): OpponentChoice = OpponentChoice.values().firstOrNull { it.code == value }!!
-    }
-}
-
-enum class ResponseChoice(val code: String) {
-    Rock("X"),
-    Paper("Y"),
-    Scissors("Z");
-
-    companion object {
-        infix fun from(value: String): ResponseChoice = ResponseChoice.values().firstOrNull { it.code == value }!!
-    }
-}
