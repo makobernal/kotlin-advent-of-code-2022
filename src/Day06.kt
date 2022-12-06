39fdb371a4ef97567d4c2efda2fdf9c8ee350ef0
Marcos Bernal <marcos.bernal@casumo.com>
2022-12-06 17:19:19 +0100

    Day 6 - Solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,15 +1,31 @@
-
+fun findIndex(list: List<String>, condition: (String) -> Boolean): Int {
+    for (i in list.indices) {
+        if (condition(list[i])) {
+            return i
+        }
+    }
+    return -1
+}
 
 fun main() {
 
 
+    fun solutioner(input: List<String>, windowSize: Int): Int {
+        val indexWhereThisHappens = findIndex(
+            input[0].windowed(windowSize)
+        ) {
+            it.toSet().size == it.length
+        }
+        return indexWhereThisHappens + windowSize
+    }
+
     fun part1(input: List<String>): Int {
-        return 8
+        return solutioner(input, 4)
     }
 
 
     fun part2(input: List<String>): Int {
-        return 13
+        return solutioner(input, 14)
     }
 
 
@@ -17,8 +33,8 @@ fun main() {
     val testInput = readInput("Day06_test")
     val testResult = part1(testInput)
     val testResult2 = part2(testInput)
-    check(testResult == 7 ) { "wrong input $testResult is not 7" }
-    check(testResult2 == 12) { "wrong input $testResult is not XXX" }
+    check(testResult == 7) { "wrong input $testResult is not 7" }
+    check(testResult2 == 19) { "wrong input $testResult is not XXX" }
 
     val input = readInput("Day06")
     val result1 = part1(input)
